b28356de125b8e4a3761d762d300686d0778ddc8
unknown <pablorilh05@gmail.com>
2023-01-05 10:04:12 -0300

    graficar una configuracion arbitraria

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ function createGame(){
 }
 
 ;(function(){
+    'use strict'
     var game = {
         word:'ALURA',
         parte: 1,
@@ -19,12 +20,38 @@ function createGame(){
         wrong: document.getElementById('wrong-word')
     }
 
-    //Actualizar imagen del ahorcado
     function draw(game){
-        $hanged = $html.hanged
-        $hanged.src = 'img/partes/0' + game.parte + '.png'
-    }
+    //Actualizar imagen del ahorcado
+        var $elem
+        $elem = $html.hanged
+        $elem.src = 'img/partes/0' + game.parte + '.png'
+    
+    //Creación de letras adivinadas
+        var word = game.word
+        var right = game.right
+        $elem = $html.right
+        for (let letter of word){
+            let $span = document.createElement('span')
+            let $txt = document.createTextNode('')
+            if (right.indexOf(letter) >= 0) {
+                $txt.nodeValue = letter
+            }
+            $span.setAttribute('class', 'letter right')
+            $span.appendChild($txt)
+            $elem.appendChild($span)
+        }
 
+        //Creación de letras erradas
+        var wrong = game.wrong
+        $elem = $html.wrong
+        for (let letter of wrong){
+            let $span = document.createElement('span')
+            let $txt = document.createTextNode(letter)
+            $span.setAttribute('class', 'letter wrong')
+            $span.appendChild($txt)
+            $elem.appendChild($span)
+        }
+    }
     
     draw(game)
 }())
