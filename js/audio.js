74a8a1290222e25fe9fd528d94dd69fe29970eb5
Pablo <87953663+PabloR94@users.noreply.github.com>
2023-01-19 19:01:46 -0300

    Modificaciones menores y adaptacion a dispositivos moviles

--- js/audio.js
+++ js/audio.js
@@ -22,4 +22,25 @@ function sound(){
     volumen.addEventListener("mouseout", function(){
         volumen.style.display= "none";
     })
+
+    //Dispositivos moviles
+    sound.addEventListener("touchstart", function(){
+        volumen.style.display= "flex";
+        volumen.addEventListener("touchmove", function(){
+        let vol = this.value
+        music1.volume = vol
+        music2.volume = vol
+
+        let speaker = document.getElementById('speaker')
+        if (vol == 0){
+            speaker.src = 'img/sonidoOff.png'
+        } else{
+            speaker.src = 'img/sonidoOn.png'
+        }
+        })
+    })
+
+    volumen.addEventListener("touchend", function(){
+        volumen.style.display= "none";
+    })
 }
